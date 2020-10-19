49e6ffb8d9e3b4e6dc98f5e7cd6241cf33dfc7ce
J John <Jsmjohn@hotmail.com>
2020-10-19 15:30:12 +0200

    Thor Animations
    -Dramatic Death
    -Implementing Foot Particles Thor & Draugr

---
+++ Valhalla/Assets/SC_BodyParticles.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SC_BodyParticles : MonoBehaviour
+{
+    public ParticleSystem leftFoot;
+    public ParticleSystem rightFoot;
+
+    public void RightFoot()
+    {
+        if(rightFoot != null)
+        {
+            rightFoot.Play();
+        }
+        //Add Sound
+    }
+
+    public void LeftFoot()
+    {
+        if(leftFoot != null)
+        {
+            leftFoot.Play();
+        }
+        //Add Sound
+    }
+
+}
