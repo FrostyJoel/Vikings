d3511717f6dc57479ae8a5aa5dd44a6b241dd3e6
J John <Jsmjohn@hotmail.com>
2020-10-22 14:28:05 +0200

    Minimap Arrow Icon
    -Arrow Rotates To nearest Enemy

---
+++ Valhalla/Assets/SC_MinimapArrow.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SC_MinimapArrow : MonoBehaviour
+{
+    public float turnSpeed;
+    public float arrowDistance;
+    public GameObject actualArrow;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (SC_GameManager.single.gameStart && SC_GameManager.single.enemies.Count > 0)
+        {
+            RotateToNearestEnemy();
+        }
+    }
+
+    private void RotateToNearestEnemy()
+    {
+        Vector3 nearestEnemyPos = GetNearestEnemy().transform.position;
+        Debug.Log(nearestEnemyPos);
+        Vector3 playerPos = SC_TopDownController.single.transform.position;
+
+        float dis = Vector3.Distance(nearestEnemyPos, playerPos);
+        if(dis >= arrowDistance)
+        {
+            if (!actualArrow.activeSelf)
+            {
+                actualArrow.SetActive(true);
+            }
+            Vector3 dir = nearestEnemyPos - playerPos;
+            Quaternion lookRotation = Quaternion.LookRotation(dir);
+            Vector3 rotation = Quaternion.Lerp(transform.rotation, lookRotation, Time.deltaTime * turnSpeed).eulerAngles;
+            transform.rotation = Quaternion.Euler(90f,rotation.y,0f);
+        }
+        else
+        {
+            if (actualArrow.activeSelf)
+            {
+                actualArrow.SetActive(false);
+            }
+        }
+    }
+
+    private SC_EnemyStats GetNearestEnemy()
+    {
+        SC_EnemyStats bestTarget = null;
+        float closestDistanceSqr = Mathf.Infinity;
+        Vector3 currentPosition = transform.position;
+        foreach (SC_EnemyStats potentialTarget in SC_GameManager.single.enemies)
+        {
+            Vector3 directionToTarget = potentialTarget.transform.position - currentPosition;
+            float dSqrToTarget = directionToTarget.sqrMagnitude;
+            if (dSqrToTarget < closestDistanceSqr)
+            {
+                closestDistanceSqr = dSqrToTarget;
+                bestTarget = potentialTarget;
+            }
+        }
+
+        return bestTarget;
+    }
+}
